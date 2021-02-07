68ce2ca89b18f70ecbef641b144bde3e511e35ce
mmulko <mykhailo.mulko@ucu.edu.ua>
2021-02-07 11:53:38 +0200

    Added functions to puzzle.py

---
+++ puzzle.py
@@ -0,0 +1,82 @@
+def validate_board(player_board):
+    """
+    Smth
+    """
+    if board_row_check(player_board) is False:
+        return False
+    elif board_row_check(row_to_column_list(player_board)) is False:
+        return False
+    elif check_colors(player_board) is False:
+        return False
+    else:
+        return True
+
+
+def board_row_check(board):
+    """
+    Smth
+    """
+    for num in range(len(board)):
+        row = board[num]
+        r_list = list(row)
+        if row_check(r_list) is False:
+            return False
+    return True
+
+
+def row_to_column_list(row_board):
+    """
+    Smth
+    """
+    f_list = []
+    for num_2 in range(len(row_board)):
+        c_list = []
+        for num in range(len(row_board)):
+            row = row_board[num]
+            row_list = list(row)
+            c_list.append(row_list[num_2])
+        f_list.append(c_list)
+    return f_list
+
+
+def row_check(r_list):
+    """
+    Smth
+    """
+    num_list = []
+    for num_2 in range(len(r_list)):
+        if r_list[num_2].isdigit():
+            num_list.append(int(r_list[num_2]))
+    if len(num_list) > 1:
+        for num_3 in range(len(num_list)):
+            for times in range(1, len(num_list) - num_3):
+                if num_list[num_3] == num_list[num_3 + times]:
+                    return False
+
+
+def check_colors(a_board):
+    """
+    Smth
+    """
+    for num in range(len(a_board)):
+        c_list = row_to_column_list(a_board)[num]
+        row = a_board[len(a_board) - 1 - num]
+        r_list = list(row)
+        f_list = c_list + r_list
+        if row_check(f_list) is False:
+            return False
+        else:
+            return True
+
+
+print(validate_board([
+    "****1****",
+    "*** 2****",
+    "**  3****",
+    "*   4****",
+    "    56781",
+    "        *",
+    "2      **",
+    "      ***",
+    "3 4  ****"
+    ]))
